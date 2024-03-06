05220b13ad7393cfa079587b7e9aef238cac49ab
tjorged <tiagoscp2020@gmail.com>
2024-03-06 16:16:52 +0000

    Homework

--- sources/parser.c
+++ sources/parser.c
@@ -65,7 +65,7 @@ int	add_token(char *line, t_token **token, int x1, int x2)
 	return (1);
 }
 
-int	go_to_quote(char *line, int	*i, char quote, int *x2)
+int	iter_quote(char *line, int	*i, char quote, int *x2)
 {
 	while (line[(*i)])
 	{
@@ -80,143 +80,73 @@ int	go_to_quote(char *line, int	*i, char quote, int *x2)
 }
 int	print_quotes(t_token **head, char *line, int *x, int *i)
 {
-	if (!go_to_quote(line, i, line[(*i)], &x[1]))
+	x[0] = (*i);
+	if (!iter_quote(line, i, line[(*i)], &x[1]))
 		return (0);
 	printf ("x1= %i\nx2= %i\n\n", x[0], x[1]);
 	if (!add_token(line, head, x[0], x[1]))
 		return (0);
-	(*i)++;
-	x[0] = (*i);
+	x[0] = (*i) + 1;
 	return (0);
 }
 
-int iter_spaces(t_token **head, char *line, int *x, int *i)
+void iter_spaces(char *line, int *x, int *i)
 {
 	while (is_space(line[(*i)]))
-	{
 		(*i)++;
-		if (line[(*i)] == '"' || line[(*i)] == '\'')
-		{
-			x[0] = (*i);
-			print_quotes(head, line, x, i);
-		}
-	}
-	return (1);
+	x[0] = (*i);
 }
 
-int	iter_arg(t_token **head, char *line, int *x, int *i)
+int	iter_chars(t_token **head, char *line, int *x, int *i)
 {
 	while (!is_space(line[(*i)]) && line[(*i)])
 	{
-		if (line[(*i)] == '"' || line[(*i)] == '\'')
+		if ( line[(*i)] == '|' || line[(*i)] == '<' || line[(*i)] == '>')
+		{
+			x[1] = (*i) - 1;
+			if (x[0] <= x[1] && !add_token(line, head, x[0], x[1]))
+				return (0);
+			if (!add_token(line, head, *i, *i))
+				return (0);
+			x[0] = (*i) + 1;
+		}
+		else if (line[(*i)] == '"' || line[(*i)] == '\'')
 		{
 			x[1] = (*i) - 1;
 			printf ("x1= %i\nx2= %i\n\n", x[0], x[1]);
-			if (!add_token(line, head, x[0], x[1]))
+			if (x[0] <= x[1] && !add_token(line, head, x[0], x[1]))
 				return (0);
-			x[0] = (*i);
 			print_quotes(head, line, x, i);
 		}
 		(*i)++;
 	}
+	x[1] = (*i) - 1;
 	return (1);
 }
 
-int tokenizer(t_token **head, char *line, int i)
+int tokenizer(t_token **head, char *line)
 {
+	int	i;
 	int	x[2];
 
+	i = 0;
 	x[0] = 0;
 	x[1] = 0;
 	while (line[i])
 	{
-		if (line[i] == '"' || line[i] == '\'')
-		{
-			x[0] = i;
-			print_quotes(head, line, x, &i);
-		}
-		iter_spaces(head, line, x, &i);
-		x[0] = i;
-		iter_arg(head, line, x, &i);
-		x[1] = i - 1;
-		printf("x2= %i!\n", x[1]);
-		if (x[0] <= x[1] && !(line[i] == '"' || line[i] == '\'') && !is_space(line[x[1]]))
+	//	if (line[i] == '"' || line[i] == '\'')
+	//		print_quotes(head, line, x, &i);
+		iter_spaces(line, x, &i);
+		iter_chars(head, line, x, &i);
+		if (x[0] <= x[1] && line[x[1]] != '\0' && !is_space(line[x[1]]))
 		{
 			printf ("(1)\nx1= %i\nx2= %i\n\n", x[0], x[1]);
 			if (!add_token(line, head, x[0], x[1]))
 				return (0);
-			x[0] = 0;
-			x[1] = 0;
 		}
 	}
 	return (1);
 }
-
-// int tokenizer(t_token **head, char *line, int x1, int x2)
-// {
-// 	int			i;
-
-// 	i = 0;
-// 	while (line[i])
-// 	{
-// 		if (line[i] == '"' || line[i] == '\'')
-// 		{
-// 			x1 = i;
-// 			if (!go_to_quote(line, &i, line[i], &x2))
-// 				return (0);
-// 			printf("1\nx1= %d\nx2= %d\n\n",x1 ,x2);
-// 			if (!add_token(line, head, x1, x2))
-// 				return (0);
-// 			i++;
-// 			x1 = i;
-// 		}
-// 		while (is_space(line[i]))
-// 		{
-// 			i++;
-// 			if (line[i] == '"' || line[i] == '\'')
-// 			{
-// 				x1 = i;
-// 				if (!go_to_quote(line, &i, line[i], &x2))
-// 					return (0);
-// 				printf("2\nx1= %d\nx2= %d\n\n",x1 ,x2);
-// 				if (!add_token(line, head, x1, x2))
-// 					return (0);
-// 				i++;
-// 				x1 = i;
-// 			}
-// 		}
-// 		x1 = i;
-// 		while (!is_space(line[i]) && line[i])
-// 		{
-// 			if (line[i] == '"' || line[i] == '\'')
-// 			{
-// 				x2 = i - 1;
-// 				printf("3\nx1= %d\nx2= %d\n\n",x1 ,x2);
-// 				if (!add_token(line, head, x1, x2))
-// 					return (0);
-// 				x1 = i;
-// 				if (!go_to_quote(line, &i, line[i], &x2))
-// 					return (0);
-// 				printf("4\nx1= %d\nx2= %d\n\n",x1 ,x2);
-// 				if (!add_token(line, head, x1, x2))
-// 					return (0);
-// 				i++;
-// 				x1 = i;
-// 			}
-// 			i++;
-// 		}
-// 		x2 = i - 1;
-// 		if (x1 <= x2 && !(line[i] == '"' || line[i] == '\'') && !is_space(line[i]))
-// 		{
-// 			printf("5\nx1= %d\nx2= %d\n\n",x1 ,x2);
-// 			if (!add_token(line, head, x1, x2))
-// 				return (0);
-// 			x1 = 0;
-// 			x2 = 0;
-// 		}
-// 	}
-// 	return (1);
-// }
 
 int	parser()
 {
@@ -227,7 +157,7 @@ int	parser()
 	token = NULL;
 	line = readline(NULL);
 	printf("\n");
-	if(!tokenizer(&token, line, 0))
+	if(!tokenizer(&token, line))
 		return (destroy_tokens(token), 0);
 	test = token;
 	while (test)
